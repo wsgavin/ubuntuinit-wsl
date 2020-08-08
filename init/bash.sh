3335d69a5d5eb5cdc6824081af686189bf6c46c0
wsgavin <warren@dubelyoo.com>
2020-08-07 19:21:05 -0500

    Updated

--- init/bash.sh
+++ init/bash.sh
@@ -4,3 +4,13 @@ echo
 printf "Initializing home directory with dotfiles..."
 cp -r home/.[!.]* ~/
 echo "done."
+
+
+cat << EOT >> "${HOME}"/.bashrc
+
+#### bash_prompt ####
+
+source "${HOME}"/.bash_prompt
+
+#### bash_prompt ####
+EOT
