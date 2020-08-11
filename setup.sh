f97d4737d9bed8838f9df26933c8c543df3fe4be
Warren Gavin <warren@dubelyoo.com>
2020-08-11 07:50:43 -0500

    Updated

--- setup.sh
+++ setup.sh
@@ -10,7 +10,6 @@ unset git_email
 . init/banner.sh
 . init/check.sh
 . init/input.sh
-. init/repos.sh
 
 # Initializing apt
 sudo apt update
@@ -21,19 +20,13 @@ sudo apt upgrade -y
 # While these installs could by done in one line, I just felt it was easier
 # to read and update.
 #
-sudo apt install git -y
+sudo apt install git -y # for older Ubuntu distros
 sudo apt install vim -y
 sudo apt install nmap -y
-#sudo apt install moreutils -y
-sudo apt install curl -y
+sudo apt install curl -y # for older Ubuntu distros
 sudo apt install ctags -y
-#sudo apt install atom -y
-sudo apt install google-chrome-stable -y
 sudo apt install shellcheck -y
 sudo apt install xclip -y
-#sudo apt install sqlite3 -y
-
-sudo snap install --classic code
 
 sudo apt install build-essential -y
 sudo apt install linux-headers-"$(uname -r)" -y
@@ -57,13 +50,10 @@ sudo apt autoclean
 sudo apt autoremove -y
 
 # Initializing scripts...
-# . init/gnome.sh
 . init/git.sh
 . init/vim.sh
 . init/bash.sh
 . init/ssh.sh
-# . init/nodejs.sh
-# . init/atom.sh
 . init/ruby.sh
 . init/python.sh
 . init/aws.sh
